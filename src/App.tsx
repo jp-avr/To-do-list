14a3398548f147699ffc6514abdbea39c28e3609
jp-avr <joaopedroazevedoreisveras@gmail.com>
2024-07-25 14:39:44 -0300

    Lógica de mudar as cores e as respectivas imagens completas

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,17 @@ import { useState, useEffect } from 'react';
 function App() {
     const [corAtual, setCorAtual] = useState<string>('');
 
+    //ASSOCIANDO AS CORES COM AS RESPECTIVAS IMAGENS
+    const imagensPorCor: Record<string, string> = {
+        '#ADBDDC': 'src/assets/blue-note.png',
+        '#FE9595': 'src/assets/pink-note.png',
+        '#C7ADDC': 'src/assets/purple-note.png',
+        '#FCB890': 'src/assets/orange-note.png',
+        '#FFE3A7': 'src/assets/yellow-note.png',
+    };
+
+    const imagemAtual = imagensPorCor[corAtual] || 'src/assets/orange-note.png'; // orange-note.png é usada como imagem padrão
+
     useEffect(() => {
         // Atualiza o estilo do body ao mudar a cor atual
         document.body.style.backgroundColor = corAtual;
@@ -21,7 +32,7 @@ function App() {
                     <div className="retangulo-amarelo" onClick={() => setCorAtual('#FFE3A7')}></div>
                 </div>
                 <div>
-                    <img className="imagem_note" src="src/assets/orange-note.png" alt="Bloco de notas colorido" />
+                    <img className="imagem_note" src={imagemAtual} alt="Bloco de notas colorido" />
                 </div>
 
             </div>
